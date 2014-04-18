0e1fa93cb84e6af093fc3ea44d51e8750adaf772
Austin Hesterly <austin.hesterly@gmail.com>
2014-04-17 21:52:41 -0600

    Completed Steps 1-4

---
+++ app/scripts/controllers/blog.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('stoogeBloggerzApp')
+  .controller('BlogCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+    $scope.blogPosts = [
+    { title: 'Moe says...', body: '"Why I oughta..."', author: 'Moe'},
+    { title: 'Curly says...', body: '"Nyuk Nyuk Nyuk!"', author: 'Curly'},
+    { title: 'Moe says...', body: '"You nitwit!"', author: 'Moe'},
+    { title: 'Lary says...', body: 'What\'s the idea?"', author: 'Larry'}
+    ]
+  });
---
+++ app/scripts/services/author.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('stoogeBloggerzApp')
+  .service('Author', function Author() {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
